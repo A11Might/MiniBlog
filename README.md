bdb04a11f2c9f59681e71164b3649364c15050f8
qihang hu <qihanghu@foxmail.com>
2020-03-11 14:25:29 +0800

    finish front end pages.

---
+++ README.md
@@ -0,0 +1,10 @@
+# MiniBlog
+
+目前写完前端页面，展示如下：
+
+
+![index](pics/register.png)
+
+![index](pics/login.png)
+
+![index](pics/index.png)
---
+++ src/main/java/com/xidian/miniblog/MiniBlogApplication.java
@@ -0,0 +1,16 @@
+package com.xidian.miniblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qhhu
+ */
+@SpringBootApplication
+public class MiniBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBlogApplication.class, args);
+	}
+
+}
